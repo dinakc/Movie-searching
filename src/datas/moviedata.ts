cc7696a674c48aa39b88ca6656976c4545935595
Ashma Upreti <ashma.upreti99@gmail.com>
2023-07-14 19:39:13 -0400

    complete search funcationality

---
+++ src/datas/moviedata.ts
@@ -0,0 +1,27 @@
+export const movies = [
+  {
+    name: "The Shawshank Redemption",
+    image_url:
+      "https://resizing.flixster.com/p1QD4TS2-Hu27jlKzz7fZaL2kfo=/fit-in/80x126/v2/https://flxt.tmsimg.com/assets/p15987_p_v12_at.jpg",
+  },
+  {
+    name: "Pulp Fiction",
+    image_url:
+      "https://resizing.flixster.com/2d_Bi_Z049s70Y6MhuRePHvJVm0=/fit-in/80x126/v2/https://flxt.tmsimg.com/assets/p15684_p_v8_af.jpg",
+  },
+  {
+    name: "The Breakfast Club",
+    image_url:
+      "https://resizing.flixster.com/EAv7n0unYb1DCn8-W8jAG7do2R8=/fit-in/80x126/v2/https://flxt.tmsimg.com/assets/p8670_p_v8_ab.jpg",
+  },
+  {
+    name: "Scent of a women",
+    image_url:
+      "https://resizing.flixster.com/6Z_KzdNA0LKn4UVlBPFWyiktozc=/fit-in/80x126/v2/https://flxt.tmsimg.com/assets/p14462_p_v8_am.jpg",
+  },
+  {
+    name: "Beautiful Boy",
+    image_url:
+      "https://resizing.flixster.com/953LqT2jYbPdpE_23_eCHqLb1dY=/fit-in/80x126/v2/https://flxt.tmsimg.com/assets/p15193596_p_v8_ak.jpg",
+  },
+];
