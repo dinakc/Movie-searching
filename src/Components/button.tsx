94d86e90a47f3d1dc485ec5f6fcf103baf7fdb17
Deena KC <dinakc14@gmail.com>
2023-07-14 20:23:28 -0400

    implement handle click

--- src/Components/button.tsx
+++ src/Components/button.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import Movie from "../pages/movie";
 
 function Button() {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/movies");
+  }
   return (
     <div>
-      <button className=" bg-black text-white w-64 h-16 rounded-md text-2xl">
-        {" "}
+      <button
+        onClick={handleClick}
+        className=" bg-black text-white w-64 h-16 rounded-md text-2xl"
+      >
         Watch Movies!
       </button>
     </div>
